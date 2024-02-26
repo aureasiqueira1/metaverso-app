6a9a53f8a556c1070ce77fe0e8b6ba3478057d6a
aureasiqueira1 <aurea@gec.inatel.br>
2024-02-26 14:42:03 -0300

    arrumando importações

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import Image from "next/image";
 import styles from "./page.module.css";
-import Background from "@/pages/Background";
-import Menu from "@/pages/Menu";
+import Background from "../pages/Background";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
-import AvatarPage from "@/pages/Avatar";
+import AvatarPage from "../pages/Avatar";
 
 export default function Home() {
   return (
